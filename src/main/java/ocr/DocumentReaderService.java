7964c69419d20ff8e84eaa622d8b1bcbd6d81c89
Aman Singh Bhandari <am727005@dal.ca>
2022-06-20 22:47:31 -0300

    add project

---
+++ src/main/java/ocr/DocumentReaderService.java
@@ -0,0 +1,4 @@
+package ocr;
+
+public class DocumentReaderService {
+}
---
+++ src/main/java/ocr/model/PassportModel.java
@@ -0,0 +1,98 @@
+package ocr.model;
+
+/**
+ * Passport pojo class to store the ket-value pairs in passport
+ */
+
+public class PassportModel {
+    private String given_name;
+    private String surname;
+    private String countryCode;
+    private String passportNumber;
+    private String sex;
+    private String dob;
+    private String placeOfBirth;
+    private String placeOfIssue;
+    private String dateOfIssue;
+    private String dateOfExpiry;
+
+    public String getGiven_name() {
+        return given_name;
+    }
+
+    public void setGiven_name(String given_name) {
+        this.given_name = given_name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getCountryCode() {
+        return countryCode;
+    }
+
+    public void setCountryCode(String countryCode) {
+        this.countryCode = countryCode;
+    }
+
+    public String getPassportNumber() {
+        return passportNumber;
+    }
+
+    public void setPassportNumber(String passportNumber) {
+        this.passportNumber = passportNumber;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    public String getDob() {
+        return dob;
+    }
+
+    public void setDob(String dob) {
+        this.dob = dob;
+    }
+
+    public String getPlaceOfBirth() {
+        return placeOfBirth;
+    }
+
+    public void setPlaceOfBirth(String placeOfBirth) {
+        this.placeOfBirth = placeOfBirth;
+    }
+
+    public String getPlaceOfIssue() {
+        return placeOfIssue;
+    }
+
+    public void setPlaceOfIssue(String placeOfIssue) {
+        this.placeOfIssue = placeOfIssue;
+    }
+
+    public String getDateOfIssue() {
+        return dateOfIssue;
+    }
+
+    public void setDateOfIssue(String dateOfIssue) {
+        this.dateOfIssue = dateOfIssue;
+    }
+
+    public String getDateOfExpiry() {
+        return dateOfExpiry;
+    }
+
+    public void setDateOfExpiry(String dateOfExpiry) {
+        this.dateOfExpiry = dateOfExpiry;
+    }
+}
