f320ba16f8430926b65034bc48487595f82ac1a2
Jay Kirankumar Patel <jy977387@dal.ca>
2022-07-26 13:30:35 -0300

    [MODIFY]: uncomment command to build docker image

--- build_image_and_push_to_ecr.sh
+++ build_image_and_push_to_ecr.sh
@@ -1,17 +1,17 @@
 aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin $1.dkr.ecr.us-east-1.amazonaws.com
 
-# docker build -t digiboard-ui ./digiboard-ui/
+docker build -t digiboard-ui ./digiboard-ui/
 docker tag digiboard-ui:latest $1.dkr.ecr.us-east-1.amazonaws.com/digiboard-ui:latest
 docker push $1.dkr.ecr.us-east-1.amazonaws.com/digiboard-ui:latest
 
-# docker build -t authenticator ./authenticator/
+docker build -t authenticator ./authenticator/
 docker tag authenticator:latest $1.dkr.ecr.us-east-1.amazonaws.com/authenticator:latest
 docker push $1.dkr.ecr.us-east-1.amazonaws.com/authenticator:latest
 
-# docker build -t extractor ./extractor/
+docker build -t extractor ./extractor/
 docker tag extractor:latest $1.dkr.ecr.us-east-1.amazonaws.com/extractor:latest
 docker push $1.dkr.ecr.us-east-1.amazonaws.com/extractor:latest
 
-# docker build -t comparator ./comparator/
+docker build -t comparator ./comparator/
 docker tag comparator:latest $1.dkr.ecr.us-east-1.amazonaws.com/comparator:latest
 docker push $1.dkr.ecr.us-east-1.amazonaws.com/comparator:latest
