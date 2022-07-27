32cd4f1d2be1afaa1d2829f5a1867131cb9e103f
mavani <hr637632@dal.ca>
2022-07-26 22:19:16 -0300

    change bucket name, ui pages

--- extractor/src/main/java/com/cloudproject/utility/UtilConstant.java
+++ extractor/src/main/java/com/cloudproject/utility/UtilConstant.java
@@ -2,7 +2,7 @@ package com.cloudproject.utility;
 
 public class UtilConstant {
 
-    public static String S3_BUCKET_NAME = "digiboard-bucket";
+    public static String S3_BUCKET_NAME = "digiboard-s3-bucket";
     public static String FOLDER_NAME = "passport";
     public static String TABLE_NAME = "digiboard-data";
 }
